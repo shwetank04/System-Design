97f34d1f66a1365d7a0125f6935709790bb76c95
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-01-06 23:50:05 +0530

    Create BasePizza.java

---
+++ DecoratorDesignPattern/BasePizza.java
@@ -0,0 +1,11 @@
+package DecoratorDesignPattern;
+
+public class BasePizza implements Pizza {
+
+	@Override
+	public String bake() {
+		// TODO Auto-generated method stub
+		return "Base Pizza";
+	}
+
+}
