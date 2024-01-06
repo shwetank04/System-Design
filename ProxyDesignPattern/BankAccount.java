603dfa30666d3cdcc9e2dc5bd906061263df5fe5
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-01-06 20:50:49 +0530

    Create BankAccount.java

---
+++ ProxyDesignPattern/BankAccount.java
@@ -0,0 +1,11 @@
+package ProxyDesignPattern;
+
+public class BankAccount implements Account {
+
+	@Override
+	public void withraw() {
+		System.out.println("Money withrawed from bank account");
+
+	}
+
+}
