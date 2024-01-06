39b6510b4fa64a86edd9d7c13de176bde7e2d30b
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-01-06 22:00:05 +0530

    Create UHD4KProcessor.java

---
+++ BridgeDesignPattern/UHD4KProcessor.java
@@ -0,0 +1,11 @@
+package BridgeDesignPattern;
+
+public class UHD4KProcessor implements VideoProcessor {
+
+	@Override
+	public void process(String videoFile) {
+		System.out.println(videoFile+"in UHD4KProcessor ");
+
+	}
+
+}
