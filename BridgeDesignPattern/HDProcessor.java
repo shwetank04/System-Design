531c83c27314b120d3ca5437daffc3ea0b292339
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-01-06 21:59:50 +0530

    Create HDProcessor.java

---
+++ BridgeDesignPattern/HDProcessor.java
@@ -0,0 +1,11 @@
+package BridgeDesignPattern;
+
+public class HDProcessor implements VideoProcessor {
+
+	@Override
+	public void process(String videoFile) {
+	System.out.println(videoFile+"in HDProcessor");
+
+	}
+
+}
