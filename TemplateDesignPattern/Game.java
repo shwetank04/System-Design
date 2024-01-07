5b4bc69a0a3156eb2d89ecb3c0a961ad0bfc1e24
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-01-07 19:08:46 +0530

    Create Game.java

---
+++ TemplateDesignPattern/Game.java
@@ -0,0 +1,15 @@
+package TemplateDesignPattern;
+
+public abstract class Game {
+	public abstract void initialize();
+	
+	public abstract void startPlay();
+	
+	public abstract void endPlay();
+	
+	public void play() {
+		initialize();
+		startPlay();
+		endPlay();
+	}
+}
