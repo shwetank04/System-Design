add30c14c49bda3f61c9cd2d44219d4045a088ca
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-01-07 19:09:01 +0530

    Create Football.java

---
+++ TemplateDesignPattern/Football.java
@@ -0,0 +1,23 @@
+package TemplateDesignPattern;
+
+public class Football extends Game {
+
+	@Override
+	public void initialize() {
+		System.out.println("Initialize Football");		
+		
+	}
+
+	@Override
+	public void startPlay() {
+		System.out.println("Start Football");		
+		
+	}
+
+	@Override
+	public void endPlay() {
+		System.out.println("End Football");		
+		
+	}
+
+}
