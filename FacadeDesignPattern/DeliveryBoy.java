8bbad8223b36f515f3b7e58e7077e3608dcffbef
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-01-07 18:21:26 +0530

    Create DeliveryBoy.java

---
+++ FacadeDesignPattern/DeliveryBoy.java
@@ -0,0 +1,10 @@
+package FacadeDesignPattern;
+
+public class DeliveryBoy {
+	public void pickUp() {
+	System.out.println("Pickup");
+}
+	public void delivery() {
+		System.out.println("Delivery");
+	}
+}
